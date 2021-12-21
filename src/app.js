cb97ea0f2c784ace57ff38b422c56ead4d0f3574
houseofsam <samxsaid@gmail.com>
2021-12-21 13:54:07 -0500

    Create form input field to add items to app object options array which will be used to display list of choices on browser. Implement function to handle form submission and add delete all button which clears the app objects option array onClick so the list can be cleared.

--- src/app.js
+++ src/app.js
@@ -3,22 +3,56 @@ console.log('App.js is running');
 const app = {
   title: 'Indecision App',
   subtitle: 'Feeling indecisive? You\'ve come to the right place :)',
-  options: ['one', 'two']
+  options: []
 };
 
-// JSX
-const template = (
-  <div>
-    <h1>{app.title && app.title}</h1>
-    <p>{app.subtitle && app.subtitle}</p>
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
+// Handle form submission
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    // append form data to array
+    app.options.push(option);
+    // reset form
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+// function to clear list when Remove All button is clicked
+const removeAll = () => {
+  app.options = [];
+  render();
+};
+
+// grab html root div
 let appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// render function to be invoked upon page load and whenever data is changed (Manual Data Binding)
+const render = () => {
+  // JSX
+  const template = (
+    <div>
+      <h1>{app.title && app.title}</h1>
+      <p>{app.subtitle && app.subtitle}</p>
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={removeAll}>Remove All</button>
+      <ol>
+        <li>Item one</li>
+      </ol>
+
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  // Grab root div and render JSX inside of it.
+  ReactDOM.render(template, appRoot);
+}
+
+render();
